074aa8b1e2285e82903b5b6285e0388218507448
Lucknay <160678001+Lucknay@users.noreply.github.com>
2024-10-15 15:42:36 -0300

    Add files via upload

---
+++ prova2.py
@@ -0,0 +1,10 @@
+numero = float(input("Digite um número:"))
+
+if numero > 0:
+ print(f"{numero} é positivo");
+
+elif numero < 0:
+ print(f"{numero} é negativo");
+
+else:
+ print(f"{numero} é zero")
